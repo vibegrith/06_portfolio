93a77e82c6310dc2ac98e2f12d09b67d54aa903e
vibegrith <vibe9737@stud.kea.dk>
2022-12-28 21:42:02 +0100

    mobil_forside responsiv

---
+++ animation.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+  document.querySelector("#cirkel_container").classList.add("grow");
+  document.addEventListener("animationend", cirkelNext);
+}
+
+function cirkelNext() {
+  console.log("cirkelNext");
+  document.querySelector("#cirkel_container").classList.remove("grow");
+  document.removeEventListener("animationend", cirkelNext);
+  document.querySelector("#cirkel_container3").classList.add("grow");
+  document.addEventListener("animationend", cirkelLast);
+}
+
+function cirkelLast() {
+  console.log("cirkelLast");
+  document.removeEventListener("animationend", cirkelLast);
+  document.querySelector("#cirkel_container2").classList.add("grow");
+}
